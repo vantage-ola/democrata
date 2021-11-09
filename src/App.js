b4d28e86933f33e0ffd9ec3a79bc91f7b5ae55b8
vantage-ola <vantageola4@gmail.com>
2021-11-09 14:59:17 +0100

    Add: plain poll app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import { Footer } from './components/Footer';
 
-function App() {
-  return (
-    <>
-    
-    </>
-  );
+import Poll from 'react-polls';
+
+const questions = "Is 2021 a better year than 2020?"
+const answers = [ { option: 'Yes', votes: 23}, {option: 'No', votes: 12}]
+const styles = {
+	questionSeparator: false,
+	questionSeparatorWidth: 'question',
+	questionBold: false ,
+	questionColor: '#5e68cc',  
+	align: 'center',
+	theme: 'purple'
 }
+class App extends Component {
+	state = {
+		answers: [...answers]
+	}
+
+	handleVote = voteAnswer => {
+		const { answers } = this.state
+		const newAnswers = answers.map(answer => {
+		  if (answer.option === voteAnswer) answer.votes++
+		  return answer
+		})
+		this.setState({
+		  answers: newAnswers
+		})
+	}
+
+	render () {
+		const { answers } = this.state
+		return (
+			<>
+			
+			<Header/>
+			<div className='center'>
+				<Poll question={questions} answers={answers} onVote={this.handleVote} customStyles={styles}/>
+			</div>
+			<Footer/>
+		  </>
+		)
+  }
+};
 
 export default App;
