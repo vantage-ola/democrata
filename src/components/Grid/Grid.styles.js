f58f64df9da73eb28e08c38d71d22f5377c36814
vantage-ola <vantageola4@gmail.com>
2021-11-11 04:28:25 +0100

    Add: Grid Component

---
+++ src/components/Grid/Grid.styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  max-width: 1660px;
+  margin: 0 auto;
+  padding: 0 20px;
+
+  h1 {
+    color: var(--white);
+
+    font-size: 2rem;
+
+    @media screen and (max-width: 768px) {
+      font-size: 1.5rem;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-gap: 2rem;
+`;
